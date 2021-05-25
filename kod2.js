073cfd0aec10bbe579d8ab745957459a0f6f9174
CoaPro <srbpro18@gmail.com>
2021-05-25 17:26:17 +0200

    Update 4 | 25.5.2021.

--- kod2.js
+++ kod2.js
@@ -1,3 +1,7 @@
+/*
+25.5.2021. Uto. 17.23h
+Uvid u greške i početak ispravljanja pojedinih delova koda...
+*/
 //Odabir fajla tj. JSON niza i prikaz u txtA
 
 let txtA = document.getElementById('txtA');
@@ -79,38 +83,6 @@ const prikazNeNulaElemenata = () => {
     txtB.value = e;
 }
 
-//13.5.2021. Čet. 
-
-/*
-Napraviti novi niz od elementa JSON niza. Indeksi novog niza bi trebalo biti
-vrednosti "All packets".
-
-Jedinstveni novi niz bi imao:
-(a) 12265 elemenata
-(b) 1334 elemenata
-
-Primer: 
-(a)
-let noviNiz = new Array(prikazNiza.f);
-(b)
-let noviNeNultiNiz = new Array(prikazNeNulaElemenataNiza.f);
-
-A elementi bi trebalo biti: 
-(a)
-
-(b)
-*/
-
-//Prikaz niza sa svim elementima
-
-const noviNiz = () => {
-    
-    const niz = JSON.parse(txtA.value);
-    const noviNiz = niz.map(({'All Packets': vrednost}) => vrednost );
-    txtC.value = noviNiz;
-    console.log(noviNiz);
-    document.getElementById('brPodnizova').innerHTML = `Broj elemenata niza tj. vremenske serije je:  ${noviNiz.length}`;
-    };
 
 // Podela niza sa svim elementima na podnizove sa četiri elementa
 
@@ -192,39 +164,7 @@ const podelaNniza = () => {
         
     }
 
-    /*
-
-    Ideja funkcionisanja podnizova: 
-
-    a[0] = noviNeNultiNiz.slice(0, 0+4);
-    a[4] = noviNeNultiNiz.slice(4, 4+4);
-    a[8] = noviNeNultiNiz.slice(8, 8+4);
-    a[12] = noviNeNultiNiz.slice(12, 12+4);
-    a[16] = noviNeNultiNiz.slice(16, 16+4);
-    ...
-
-    tj.
-
-    a[0] = noviNeNultiNiz.slice(0, 4);
-    a[4] = noviNeNultiNiz.slice(4, 8);
-    a[8] = noviNeNultiNiz.slice(8, 12);
-    a[12] = noviNeNultiNiz.slice(12, 16);
-    a[16] = noviNeNultiNiz.slice(16, 20);
-
-     */
-
-    //Test
-
-    /*
-    console.log(noviNeNultiNiz.slice(0, 4));
-    console.log(noviNeNultiNiz.slice(4, 8));
-    console.log(noviNeNultiNiz.slice(12, 16));
-    console.log(noviNeNultiNiz.slice(16, 20));
-    console.log(noviNeNultiNiz.slice(20, 24));
-    console.log(noviNeNultiNiz.slice(24, 28));
-    
-    console.log(`Broj podnizova je ${br}`);
-    */
+   
 
     document.getElementById('brPodnizova').innerHTML = `Broj podnizova je sada:  ${br}`;
 
@@ -300,94 +240,6 @@ const proracun = () => {
     txtN.value = N;
     txtZbir.value = zbirVrSerije;
     txtSrVr.value = srVrVrSerije;
-
-    /* 
-    Podela vremenske serije (od 1334 elementa) na podnizove od četiri elementa...
-    Za svaki podniz definisati sledeće vrednosti i metode:
-
-    x1 = a[0];
-    x2 = a[1];
-    x3 = a[2];
-    x4 = a[3];
-
-    w1 = x1 - SrVr;
-    w2 = x1 + x2 - 2*SrVr;
-    w3 = x1 + x2 + x3 - 3*SrVr;
-    w4 = x1 + x2 + x3 + x4 - 4*SrVr;
-
-    */
-
-    //Test na prvom isečku niza: 
-
-
-    // 20.5.2021. Čet. 
-
-    //Isečak niza tj. vremenska serija od četiri elementa
-
-    /*
-    a = noviNeNultiNiz.slice(0, 4); 
-
-    let zbirA = 0;
-    let srVrA = 0;
-    let nA = 0;
-
-    for(let i = 0; i < a.length; i++){
-        nA++;
-        zbirA += a[i];
-
-    }
-
-    
-   //Određivanje koeficijenata x1, x2, x3, x4
-
-   x1 = a[0];
-   x2 = a[1];
-   x3 = a[2];
-   x4 = a[3];
-   
-   //Srednja vrednost vremenske serije od četiri elementa
-
-   srVrA = (zbirA / nA);
-
-    //Određivanje vrednosti koeficijenata w1, w2, w3, w4, tj. kumulativnih devijacija grupe
-
-    w1 = x1 - srVrA;
-    w2 = x1 + x2 - 2*srVrA;
-    w3 = x1 + x2 + x3 - 3*srVrA;
-    w4 = x1 + x2 + x3 + x4 - 4*srVrA;
-
-    let Rn, maxVr, minVr;
-
-    maxVr = Math.max(w1, w2, w3, w4);
-    minVr = Math.min(w1, w2, w3, w4);
-    
-    //R(4) - opseg grupe
-
-    Rn = maxVr - minVr;
-
-    //S(4) - standardna devijacija
-
-    let Sn_, Sn;
-
-    Sn_ = Math.sqrt((1/4)*((x1 - srVrA)**2 + (x2 - srVrA)**2 + (x3 - srVrA)**2 + (x4 - srVrA)**2));
-    Sn = Number(Sn_.toFixed(2));
-
-    let RS, logN, logRS;
-
-    //RS statistika
-
-    RS = Number((Rn/Sn).toFixed(2));
-
-    //Vrednosti potrebne za vizuelizaciju podataka
-
-    logN = Number(Math.log10(nA).toFixed(2));
-    logRS = Number(Math.log10(RS).toFixed(2));
-
-
-    //console.log(nA, zbirA, a[0], a[1], a[2], a[3], srVrA);
-    console.log(w1, w2, w3, w4, maxVr, minVr, Rn, Sn, RS, logN, logRS); 
-
-*/
 
 //Test #2
 
