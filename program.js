c654bee7a7d0b23130054d41812bdfc5e780cf78
CoaPro <srbpro18@gmail.com>
2021-06-01 12:13:16 +0200

    Update 1 | 1.6.2021.

--- program.js
+++ program.js
@@ -674,42 +674,6 @@ let txtSpod = document.getElementById('txtSpod');
 let txtRSpod = document.getElementById('txtRSpod');
 let txtLogRSpod = document.getElementById('txtLogRSpod');
 
-let graf = document.getElementById('grafik').getContext('2d');
-let vizuelizacijaPodataka = new Chart(graf, {
-    type: 'bubble',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'Vrednost',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
 
 let podniz;
 let zbirVS;
@@ -906,6 +870,44 @@ function acaaa(){
     console.log(noviNeNultiNiz);
 }
 
+function vizuelizacijaPodataka(){
+    let graf = document.getElementById('grafik').getContext('2d');
+    let vizuelizacijaPodataka = new Chart(graf, {
+        type: 'bar',
+        data: {
+            labels: ceoNizSumaLogn,
+            datasets: [{
+                label: 'Vrednost',
+                data: [ceoNizSumaLognLogRS],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
 const pro = () => {
 
     /* Definisanje promenljivih: */
@@ -1008,4 +1010,6 @@ const pro = () => {
     console.log(`Hurstov parametar je: ${H}`);
 
     txtHurst.value = H;
+
+    vizuelizacijaPodataka();
 };
