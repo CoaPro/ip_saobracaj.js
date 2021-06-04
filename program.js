de27f76c622e666b905f2622d379f2ecb77306fc
CoaPro <srbpro18@gmail.com>
2021-06-04 12:42:25 +0200

    Update 1 | 4.6.2021.

--- program.js
+++ program.js
@@ -955,9 +955,47 @@ za izračunavanje Hurstovog parametra i vizuelizaciju podataka...
 function acaaa(){
     console.log(noviNeNultiNiz);
 }
-//test
-function vizuelizacijaPodataka(){
+
+function RSstatistika(){
     let graf = document.getElementById('grafik').getContext('2d');
+    let vizuelizacijaPodataka = new Chart(graf, {
+        type: 'bar',
+        data: {
+            labels: ceoNizLogn, //ceoNizLogn, testAniz
+            datasets: [{
+                label: 'Vrednost',
+                data: ceoNizLogRS, //ceoNizLogRS
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });   
+}
+
+function vizuelizacijaSvihPodataka(){
+    let graf = document.getElementById('grafik2').getContext('2d');
     let vizuelizacijaPodataka = new Chart(graf, {
         type: 'bar',
         data: {
@@ -1080,8 +1118,10 @@ const pro = () => {
 
     console.log(`Hurstov parametar je: ${H}`);
     txtHurst.value = H;
+    
+    RSstatistika();
+    vizuelizacijaSvihPodataka();
 
-    vizuelizacijaPodataka();
     } else {
 
         txtHurst.value = 'Desila se neuobičajena greška. Klikinite na dugme Obrisati i pokušajte ponovo. Neki od mogućih uzroka su pojava više NaN vrednosti ili semantičke greške u kodu.';
